9dcbf8187b466c278002f482457d3152e0ce1571
SergeiSOficial <geos2@mail.ru>
2021-03-14 22:04:25 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AnaBot
 Simple telegram bot to control esp32 servo
+
+Forked from https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot/blob/master/examples/ESP32/FlashLED/FlashLED.ino
+
+Tutorial https://randomnerdtutorials.com/telegram-control-esp32-esp8266-nodemcu-outputs/
+
+Need install https://platformio.org/ and https://code.visualstudio.com/
+
---
+++ src/AnaBot.ino
@@ -0,0 +1,166 @@
+/*******************************************************************
+    A telegram bot for your ESP32 that controls the 
+    onboard LED. The LED in this example is active low.
+    Parts:
+    ESP32 D1 Mini stlye Dev board* - http://s.click.aliexpress.com/e/C6ds4my
+    (or any ESP32 board)
+      = Affilate
+    If you find what I do useful and would like to support me,
+    please consider becoming a sponsor on Github
+    https://github.com/sponsors/witnessmenow/
+    Written by Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Tindie: https://www.tindie.com/stores/brianlough/
+    Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include <ServoEasing.h>
+
+//#include <PinDefinitionsAndMore.h>
+/*
+ * Pin mapping table for different platforms
+ *
+ * Platform     Servo1      Servo2      Servo3      Analog
+ * -------------------------------------------------------
+ * AVR + SAMD   9           10          11          A0
+ * ESP8266      14 // D5    12 // D6    13 // D7    0
+ * ESP32        5           18          19          A0
+ * BluePill     PB7         PB8         PB9         PA0
+ * APOLLO3      11          12          13          A3
+ */
+#define SERVO1_PIN 5
+
+ServoEasing Servo1;
+
+// Wifi network station credentials
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+const unsigned long BOT_MTBS = 1000; // mean time between scan messages
+
+WiFiClientSecure secured_client;
+UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+unsigned long bot_lasttime; // last time messages' scan has been done
+
+const int ledPin = LED_BUILTIN;
+int ledStatus = 0;
+
+void handleNewMessages(int numNewMessages)
+{
+  Serial.print("handleNewMessages ");
+  Serial.println(numNewMessages);
+
+  for (int i = 0; i < numNewMessages; i++)
+  {
+    String chat_id = bot.messages[i].chat_id;
+    String text = bot.messages[i].text;
+
+    String from_name = bot.messages[i].from_name;
+    if (from_name == "")
+      from_name = "Guest";
+
+    if (text == "/hi")
+    {
+      Servo1.write(0);
+      digitalWrite(ledPin, LOW); // turn the LED on (HIGH is the voltage level)
+      ledStatus = 1;
+      bot.sendMessage(chat_id, "Led is ON", "");
+    }
+
+    if (text == "/bay")
+    {
+      Servo1.write(90);
+      ledStatus = 0;
+      digitalWrite(ledPin, HIGH); // turn the LED off (LOW is the voltage level)
+      bot.sendMessage(chat_id, "Led is OFF", "");
+    }
+
+    if (text == "/status")
+    {
+      if (ledStatus)
+      {
+        bot.sendMessage(chat_id, "Led is ON", "");
+      }
+      else
+      {
+        bot.sendMessage(chat_id, "Led is OFF", "");
+      }
+    }
+
+    if (text == "/start")
+    {
+      String welcome = "Welcome to Universal Arduino Telegram Bot library, " + from_name + ".\n";
+      welcome += "This is Flash Led Bot example.\n\n";
+      welcome += "/hi : to switch the Led ON\n";
+      welcome += "/bay : to switch the Led OFF\n";
+      welcome += "/status : Returns current status of LED\n";
+      bot.sendMessage(chat_id, welcome, "Markdown");
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(ledPin, OUTPUT); // initialize digital ledPin as an output.
+  delay(10);
+  digitalWrite(ledPin, HIGH); // initialize pin as off (active LOW)
+
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting to Wifi SSID ");
+  Serial.print(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nWiFi connected. IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (Servo1.attach(SERVO1_PIN) == INVALID_SERVO)
+  {
+    Serial.println(F("Error attaching servo"));
+  }
+  /**************************************************
+  * Set servo to start position.
+  * This is the position where the movement starts.
+  *************************************************/
+  Servo1.write(0);
+
+  Serial.print("Retrieving time: ");
+  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  time_t now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    Serial.print(".");
+    delay(100);
+    now = time(nullptr);
+  }
+  Serial.println(now);
+}
+
+void loop()
+{
+  if (millis() - bot_lasttime > BOT_MTBS)
+  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages)
+    {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    bot_lasttime = millis();
+  }
+}
